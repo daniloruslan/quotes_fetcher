66b4f5e43ed553e25c04256a28417931563e5bd9
Ruslan Danila <47849927+daniloruslan@users.noreply.github.com>
2020-02-26 08:35:48 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,12 @@ setup(
   author = 'Ruslan Danila',                   # Type in your name
   author_email = 'daniloruslan@yahoo.com',      # Type in your E-Mail
   url = 'https://github.com/user/daniloruslan',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/daniloruslan/quotes_fetcher/archive/v0.2.tar.gz',    # I explain this later on
   keywords = ['exchange', 'data', 'quotes'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'yfinance',
+          'pandas',
+          'numpy',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
